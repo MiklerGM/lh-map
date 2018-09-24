a7061bb21f8cb70b8f7914ca3c42f0a20c7bbd2d
Michael Orlov <MiklerGM@gmail.com>
2018-09-24 14:31:32 +0900

    Parsing regions from map.json

---
+++ data/parseRegions.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const mapGeo = require('./map.json');
+
+const regions = {};
+const rKey = 'adm0_a3';
+
+mapGeo.features.map((f) => {
+  const adm = f.properties[rKey];
+  regions[adm] = f.properties;
+  return null;
+});
+
+const listOfRegions = Object.keys(regions);
+listOfRegions.map((r) => {
+  regions[r].owner = regions[r].region !== ''
+    ? listOfRegions.find(f => regions[f].admin === regions[r].admin)
+    : null;
+  return null;
+});
+
+fs.writeFileSync('./regions.json', JSON.stringify(regions));
