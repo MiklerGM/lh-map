4f92ecf225d4ad73c8a3e984107216e6aece816c
Dmitriy Shikhaleev <padavankun@gmail.com>
2018-09-25 13:13:41 +0300

    updated to babel 7

--- webpack.config.ssr.js
+++ webpack.config.ssr.js
@@ -34,31 +34,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
-      },
-      {
-        test: /\.less$/,
-        use: ['style-loader', { loader: 'css-loader', options: { importLoaders: 1 } }, 'less-loader']
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: ['url-loader?limit=4096&name=[name].[ext]']
-      },
-      {
-        test: /\.(woff|woff2|ttf|eot)$/,
-        loader: 'url-loader?limit=100000&name=[name].[ext]'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-      },
-      {
-        test: /\.(mp4|m4v)$/,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.md$/,
-        loader: 'markdown-with-front-matter-loader'
-      },
+      }
     ]
   }
 };
